8a9dbf340fb032f195b2e643b1e6fc1f13e0b659
celestino.bellone <celestino.bellone@gmail.com>
2014-08-07 19:31:12 +0200

    add license

--- src/main/java/io/bagarino/model/Event.java
+++ src/main/java/io/bagarino/model/Event.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of bagarino.
+ *
+ * bagarino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bagarino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bagarino.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.bagarino.model;
 
 import io.bagarino.model.user.Organization;
