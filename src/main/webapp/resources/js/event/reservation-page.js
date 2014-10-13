be34f933f6204c26e0ea49f2279c52ef43ad299a
Sylvain Jermini <sylvain.jermini@syjer.com>
2014-10-13 22:31:10 +0200

    initial client side validation

--- src/main/webapp/resources/js/event/reservation-page.js
+++ src/main/webapp/resources/js/event/reservation-page.js
@@ -7,12 +7,15 @@
 		var $form = $('#payment-form');
 	 
 		if (response.error) {
+			$(".payment-errors").show().empty();
 			// Show the errors on the form
 			// TODO: see http://stackoverflow.com/questions/23437439/non-english-texts-in-stripe-possible 
 			// use the code for handle the localization
-			$form.find('.payment-errors').text(response.error.message);
+			$form.find('.payment-errors').append("<p><strong>"+response.error.message+"</strong></p>");
 			$form.find('button').prop('disabled', false);
+			
 		} else {
+			$(".payment-errors").hide();
 			// token contains id, last4, and card type
 			var token = response.id;
 			// Insert the token into the form so it gets submitted to the server
@@ -32,6 +35,8 @@
 	
 	 
 	jQuery(function($) {
+		
+		$(".payment-errors").hide();
 		
 		function submitForm(e) {
 			var $form = $(this);
@@ -60,5 +65,53 @@
 		if(hasStripe) {
 			$('#payment-form').submit(submitForm);
 		}
+		
+		
+		
+		
+		
+		
+		// based on http://tjvantoll.com/2012/08/05/html5-form-validation-showing-all-error-messages/
+		// http://stackoverflow.com/questions/13798313/set-custom-html5-required-field-validation-message
+		var createAllErrors = function() {
+			var form = $(this);
+
+			var showAllErrorMessages = function() {
+				$(form).find('.has-error').removeClass('has-error');
+				// Find all invalid fields within the form.
+				var invalidFields = form.find( ":invalid" ).each( function( index, node ) {
+					$(node).parent().addClass('has-error');
+				});
+			};
+
+			// Support Safari
+			form.on("submit", function( event ) {
+				if (this.checkValidity && !this.checkValidity() ) {
+					$(this).find( ":invalid" ).first().focus();
+					event.preventDefault();
+				}
+			});
+
+			$("input[type=submit], button:not([type=button])", form ).on("click", showAllErrorMessages);
+
+			$("input", form).on("keypress", function(event) {
+				var type = $(this).attr("type");
+				if ( /date|email|month|number|search|tel|text|time|url|week/.test(type) && event.keyCode == 13 ) {
+					showAllErrorMessages();
+				}
+			});
+		};
+		
+		$("form").each(createAllErrors);
+		$("input,select,textarea").change(function() {
+			if($(this).is(":invalid")) {
+				$(this).parent().addClass('has-error');
+			} else {
+				$(this).parent().removeClass('has-error');
+			}
+		})
 	});
+	
+	
+	
 })();
