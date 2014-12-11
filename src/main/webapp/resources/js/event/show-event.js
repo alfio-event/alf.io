ccddb0739c6a91bae5e2ceab5004021766fc39cf
Celestino Bellone <celestino.bellone@gmail.com>
2014-12-11 19:45:19 +0100

    hide promotional code box if a promo code has been entered

--- src/main/webapp/resources/js/event/show-event.js
+++ src/main/webapp/resources/js/event/show-event.js
@@ -3,20 +3,26 @@
 	'use strict';
 	
 	$(function() {
-		
-		$("#promo-code").keydown(function(e) {
-			if (e.keyCode == 13) {
-				$("#apply-promo-codes").click();
-				return false;
-			}
-		});
-		
-		$("#apply-promo-codes").click(function() {
-			var promoCodeVal = $("#promo-code").val();
-			if(promoCodeVal != null && promoCodeVal.trim() != "") {
-				window.location.href = window.location.pathname +"?promoCode="+promoCodeVal;
-			}
-		});
+
+		var restrictedCategories = $('.ticket-category-restricted-true');
+		if(restrictedCategories.length === 0) {
+			$("#promo-code").keydown(function(e) {
+				if (e.keyCode == 13) {
+					$("#apply-promo-codes").click();
+					return false;
+				}
+			});
+
+			$("#apply-promo-codes").click(function() {
+				var promoCodeVal = $("#promo-code").val();
+				if(promoCodeVal != null && promoCodeVal.trim() != "") {
+					window.location.href = window.location.pathname +"?promoCode="+promoCodeVal;
+				}
+			});
+		} else {
+			$('#accessRestrictedTokens').hide();
+		}
+
 	});
 	
 })();
