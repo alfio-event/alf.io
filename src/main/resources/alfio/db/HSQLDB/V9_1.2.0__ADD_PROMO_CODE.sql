8d234ed3fd2eddc00116169a68e8a8dd7604aabd
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-01-08 21:30:32 +0100

    #29 data model

---
+++ src/main/resources/alfio/db/HSQLDB/V9_1.2.0__ADD_PROMO_CODE.sql
@@ -0,0 +1,37 @@
+--
+-- This file is part of alf.io.
+--
+-- alf.io is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- alf.io is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+create table promo_code (
+  id integer identity not null,
+  promo_code varchar(64) not null,
+  event_id_fk integer not null,
+  start timestamp with time zone not null, 
+  end timestamp with time zone not null,
+  discount_amount integer not null,
+  discount_type varchar(16) not null
+);
+alter table promo_code add constraint "unique_promo_code_for_event" unique(promo_code, event_id_fk);
+alter table promo_code add constraint "check_promo_code_min_length" check (char_length(promo_code) >= 7);
+alter table promo_code add constraint "check_discount_type" check (discount_type = 'FIXED_AMOUNT' OR discount_type = 'PERCENTAGE');
+alter table promo_code add constraint "check_discount_amount" check (
+CASE
+	WHEN discount_type = 'FIXED_AMOUNT' THEN discount_amount >= 0
+	WHEN discount_type = 'PERCENTAGE' THEN discount_amount >= 0 and discount_amount <= 100
+END);
+alter table promo_code add foreign key(event_id_fk) references event(id);
+
+CREATE INDEX "promo_code_event_id_fk_idx" ON promo_code(event_id_fk);
