023af01d4c14f00036769dc5df7e506a2822c454
Celestino Bellone <celestino.bellone@gmail.com>
2015-02-15 12:17:45 +0100

    fix bug on configuration GUI

--- src/main/webapp/resources/js/admin/filter/util.js
+++ src/main/webapp/resources/js/admin/filter/util.js
@@ -33,6 +33,9 @@
 
     filters.filter('mailSettingsFilter', function() {
         return function(list, mailType) {
+            if(angular.isUndefined(mailType) || mailType === null) {
+                return [];
+            }
             var referenceKey = mailType.toUpperCase();
             return _.filter(list, function(e) {
                 return e.key.toUpperCase().indexOf(referenceKey) > -1;
