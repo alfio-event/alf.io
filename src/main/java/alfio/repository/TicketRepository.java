349307c8c975cc43532deed1e57e57481a8b7309
Celestino Bellone <celestino.bellone@gmail.com>
2015-04-06 11:07:53 +0200

    order report by confirmation timestamp

--- src/main/java/alfio/repository/TicketRepository.java
+++ src/main/java/alfio/repository/TicketRepository.java
@@ -114,7 +114,7 @@ public interface TicketRepository {
 	@Query("select * from ticket where event_id = :eventId and full_name is not null and email_address is not null")
 	List<Ticket> findAllAssignedByEventId(@Bind("eventId") int eventId);
 
-    @Query("select a.*, b.id from ticket a, tickets_reservation b where a.event_id = :eventId and a.status in(" + CONFIRMED + ") and a.tickets_reservation_id = b.id order by b.id")
+    @Query("select a.*, b.confirmation_ts from ticket a, tickets_reservation b where a.event_id = :eventId and a.status in(" + CONFIRMED + ") and a.tickets_reservation_id = b.id order by b.confirmation_ts")
     List<Ticket> findAllConfirmed(@Bind("eventId") int eventId);
 
     @Query("select * from ticket where event_id = :eventId and status in(" + CONFIRMED + ") and category_id = :categoryId")
