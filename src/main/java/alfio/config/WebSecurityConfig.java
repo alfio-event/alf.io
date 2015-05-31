a11f9436e91bffa8aebe9dc9802789c0e2af1b48
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-05-31 15:59:30 +0200

    support basic auth for a limited set of the APIs for simpler mobile app integration

--- src/main/java/alfio/config/WebSecurityConfig.java
+++ src/main/java/alfio/config/WebSecurityConfig.java
@@ -17,7 +17,6 @@
 package alfio.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -26,6 +25,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
@@ -35,9 +35,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableWebMvcSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     public static final String ADMIN_API = "/admin/api";
     public static final String CSRF_SESSION_ATTRIBUTE = "CSRF_SESSION_ATTRIBUTE";
@@ -46,49 +45,87 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String OWNER = "OWNER";
     private static final String OPERATOR = "OPERATOR";
 
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
-    @Bean
-    public CsrfTokenRepository getCsrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setSessionAttributeName(CSRF_SESSION_ATTRIBUTE);
-        repository.setParameterName(CSRF_PARAM_NAME);
-        return repository;
+
+    private static class BaseWebSecurity extends  WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private DataSource dataSource;
+        @Autowired
+        private PasswordEncoder passwordEncoder;
+
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.jdbcAuthentication().dataSource(dataSource)
+                    .usersByUsernameQuery("select username, password, enabled from ba_user where username = ?")
+                    .authoritiesByUsernameQuery("select username, role from authority where username = ?")
+                    .passwordEncoder(passwordEncoder);
+        }
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from ba_user where username = ?")
-                .authoritiesByUsernameQuery("select username, role from authority where username = ?")
-                .passwordEncoder(passwordEncoder);
+    /**
+     * Basic auth configuration for Mobile App.
+     * The rules are only valid if the header Authorization is present, otherwise it fallback to the
+     * FormBasedWebSecurity rules.
+     */
+    @Configuration
+    @Order(1)
+    public static class BasicAuthWebSecurity extends BaseWebSecurity {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.requestMatcher((request) -> {
+                return request.getHeader("Authorization") != null;
+            }).sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeRequests()
+            .antMatchers(ADMIN_API + "/check-in/**").hasRole(OPERATOR)
+            .antMatchers(HttpMethod.GET, ADMIN_API + "/**").hasRole(OPERATOR)
+            .antMatchers("/**").denyAll()
+            .and().httpBasic();
+        }
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.exceptionHandling()
-            .accessDeniedPage("/session-expired")
-            .defaultAuthenticationEntryPointFor((request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED), new RequestHeaderRequestMatcher("X-Requested-With", "XMLHttpRequest"))
-            .and()
-            .csrf()
-            .csrfTokenRepository(getCsrfTokenRepository())
-            .and()
-            .authorizeRequests()
-            .antMatchers(ADMIN_API + "/organizations/new", ADMIN_API + "/users/**", ADMIN_API + "/configuration/**").hasRole(ADMIN)
-            .antMatchers(ADMIN_API + "/check-in/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
-            .antMatchers(HttpMethod.GET, ADMIN_API + "/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
-            .antMatchers(ADMIN_API + "/**").hasAnyRole(ADMIN, OWNER)
-            .antMatchers("/admin/**/export/**").hasAnyRole(ADMIN, OWNER)
-            .antMatchers("/admin/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
-            .antMatchers("/**").permitAll()
-            .and()
-            .formLogin()
-            .loginPage("/authentication")
-            .loginProcessingUrl("/authentication")
-            .failureUrl("/authentication?failed");
+    /**
+     * Default form based configuration.
+     */
+    @Configuration
+    @Order(2)
+    public static class FormBasedWebSecurity extends BaseWebSecurity {
 
+        @Bean
+        public CsrfTokenRepository getCsrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setSessionAttributeName(CSRF_SESSION_ATTRIBUTE);
+            repository.setParameterName(CSRF_PARAM_NAME);
+            return repository;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.exceptionHandling()
+                    .accessDeniedPage("/session-expired")
+                    .defaultAuthenticationEntryPointFor((request, response, ex) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED), new RequestHeaderRequestMatcher("X-Requested-With", "XMLHttpRequest"))
+                    .and()
+                    .csrf()
+                    .csrfTokenRepository(getCsrfTokenRepository())
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers(ADMIN_API + "/organizations/new", ADMIN_API + "/users/**", ADMIN_API + "/configuration/**").hasRole(ADMIN)
+                    .antMatchers(ADMIN_API + "/check-in/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
+                    .antMatchers(HttpMethod.GET, ADMIN_API + "/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
+                    .antMatchers(ADMIN_API + "/**").hasAnyRole(ADMIN, OWNER)
+                    .antMatchers("/admin/**/export/**").hasAnyRole(ADMIN, OWNER)
+                    .antMatchers("/admin/**").hasAnyRole(ADMIN, OWNER, OPERATOR)
+                    .antMatchers("/**").permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/authentication")
+                    .loginProcessingUrl("/authentication")
+                    .failureUrl("/authentication?failed");
+        }
     }
+
+
+
+
 }
