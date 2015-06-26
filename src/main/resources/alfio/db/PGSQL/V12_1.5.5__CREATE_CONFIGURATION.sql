3a5de2d18aa7be72493d035bb0bbd9d81d03fa5f
Sylvain Jermini <sylvain.jermini@syjer.com>
2015-06-26 16:46:22 +0200

    #56 refactor+add org level configuration table

---
+++ src/main/resources/alfio/db/PGSQL/V12_1.5.5__CREATE_CONFIGURATION.sql
@@ -0,0 +1,35 @@
+create table configuration_organization(
+  id serial primary key not null,
+  organization_id_fk integer not null,
+  c_key varchar(255) not null,
+  c_value varchar(2048) not null,
+  description varchar(2048)
+);
+alter table configuration_organization add constraint "unique_configuration_organization" unique(organization_id_fk, c_key);
+alter table configuration_organization add foreign key(organization_id_fk) references organization(id);
+
+create table configuration_event(
+  id serial primary key not null,
+  organization_id_fk integer not null,
+  event_id_fk integer not null,
+  c_key varchar(255) not null,
+  c_value varchar(2048) not null,
+  description varchar(2048)
+);
+alter table configuration_event add constraint "unique_configuration_event" unique(organization_id_fk, event_id_fk, c_key);
+alter table configuration_event add foreign key(organization_id_fk) references organization(id);
+alter table configuration_event add foreign key(event_id_fk) references event(id);
+
+create table configuration_ticket_category(
+  id serial primary key not null,
+  organization_id_fk integer not null,
+  event_id_fk integer not null,
+  ticket_category_id_fk integer not null,
+  c_key varchar(255) not null,
+  c_value varchar(2048) not null,
+  description varchar(2048)
+);
+alter table configuration_ticket_category add constraint "unique_configuration_ticket_category" unique(organization_id_fk, event_id_fk, ticket_category_id_fk, c_key);
+alter table configuration_ticket_category add foreign key(organization_id_fk) references organization(id);
+alter table configuration_ticket_category add foreign key(event_id_fk) references event(id);
+alter table configuration_ticket_category add foreign key(ticket_category_id_fk) references ticket_category(id);
