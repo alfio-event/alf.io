f8ea029c40e2a5820ff18f385dea425093ad287c
Celestino Bellone <celestino@exteso.com>
2015-08-04 21:10:03 +0200

    #66 - first round of redesign...

--- src/main/webapp/resources/js/event/reservation-page-complete.js
+++ src/main/webapp/resources/js/event/reservation-page-complete.js
@@ -99,6 +99,28 @@
 				});
 				return false;
 			});
+
+            var collapsibleContainer = $('.collapsible-container[data-collapse-enabled="true"]');
+            if(collapsibleContainer.length > 1) {
+                var collapsibleElement = collapsibleContainer.find('.collapsible');
+                collapsibleElement.addClass('hidden');
+                collapsibleElement.attr('aria-expanded', 'false');
+                collapsibleContainer.find('div.toggle-collapse').removeClass('hidden');
+                collapsibleContainer.find('a.toggle-collapse').click(function() {
+                    var expanded = collapsibleElement.attr('aria-expanded');
+                    if(expanded === 'true') {
+                        collapsibleElement.addClass('hidden');
+                        collapsibleElement.attr('aria-expanded', 'false');
+                        collapsibleContainer.find('span.collapse-less').addClass('hidden');
+                        collapsibleContainer.find('span.collapse-more').removeClass('hidden');
+                    } else {
+                        collapsibleElement.removeClass('hidden');
+                        collapsibleElement.attr('aria-expanded', 'true');
+                        collapsibleContainer.find('span.collapse-more').addClass('hidden');
+                        collapsibleContainer.find('span.collapse-less').removeClass('hidden');
+                    }
+                });
+            }
 		};
 
 		initListeners();
