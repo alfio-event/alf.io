541f2e4ee78d37d57f280902f81399f891084928
Celestino Bellone <celestino@exteso.com>
2015-08-09 16:14:25 +0200

    #66 - show form by default

---
+++ src/main/webapp/resources/js/event/update-ticket.js
@@ -0,0 +1,6 @@
+(function() {
+    'use strict';
+    $(function() {
+        $('.collapsible-container[data-ticket-update-page="true"] form[data-ticket-uuid]').removeClass('hide-by-default');
+    });
+})();
