d10573c3445f6f097f130fce5ddb3fd36d16cf24
Celestino Bellone <celestino@exteso.com>
2016-04-07 08:20:58 +0200

    #116 - redirect to /admin if the user is already logged in (cherry picked from commit d96e7e2)

--- src/main/java/alfio/config/WebSecurityConfig.java
+++ src/main/java/alfio/config/WebSecurityConfig.java
@@ -138,7 +138,7 @@ public class WebSecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/authentication")
-                .loginProcessingUrl("/authentication")
+                .loginProcessingUrl("/authenticate")
                 .failureUrl("/authentication?failed");
         }
     }
