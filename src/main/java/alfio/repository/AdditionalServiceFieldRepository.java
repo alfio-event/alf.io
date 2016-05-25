57564eb657bf851d5a98ac904a5485cc32071d54
Celestino Bellone <celestino@exteso.com>
2016-05-25 08:14:39 +0200

    #110 and #111: create repositories for additional service fields

--- src/main/java/alfio/repository/AdditionalServiceFieldRepository.java
+++ src/main/java/alfio/repository/AdditionalServiceFieldRepository.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of alf.io.
+ *
+ * alf.io is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * alf.io is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package alfio.repository;
 
 import alfio.model.AdditionalServiceFieldValue;
@@ -9,7 +25,7 @@ import ch.digitalfondue.npjt.QueryRepository;
 import java.util.List;
 
 @QueryRepository
-public interface AdditionalServiceFieldValueRepository {
+public interface AdditionalServiceFieldRepository extends FieldRepository {
 
     @Query("select a.additional_service_id_fk, a.ticket_field_configuration_id_fk, b.field_name, a.field_value from additional_service_field_value a, ticket_field_configuration b where a.additional_service_id_fk = :additionalServiceId and a.ticket_field_configuration_id_fk = b.id and b.context = 'ADDITIONAL_SERVICE'")
     List<AdditionalServiceFieldValue> findAllByAdditionalServiceId(@Bind("additionalServiceId") int id);
