9df6a1819404d8a772f62492cc326b4d7a29bd1d
Celestino Bellone <celestino@exteso.com>
2016-09-07 07:39:51 +0200

    do not add VAT to the final price, since it is already included

--- src/main/java/alfio/manager/CheckInManager.java
+++ src/main/java/alfio/manager/CheckInManager.java
@@ -156,7 +156,7 @@ public class CheckInManager {
         final TicketStatus ticketStatus = ticket.getStatus();
 
         if (ticketStatus == TicketStatus.TO_BE_PAID) {
-            return new TicketAndCheckInResult(ticket, new OnSitePaymentResult(MUST_PAY, "Must pay for ticket", MonetaryUtil.addVAT(MonetaryUtil.centsToUnit(ticket.getFinalPriceCts()), event.getVat()), event.getCurrency()));
+            return new TicketAndCheckInResult(ticket, new OnSitePaymentResult(MUST_PAY, "Must pay for ticket", MonetaryUtil.centsToUnit(ticket.getFinalPriceCts()), event.getCurrency()));
         }
 
         if (ticketStatus == TicketStatus.CHECKED_IN) {
