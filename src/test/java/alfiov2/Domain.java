780812d68b05c802a08ffbf17f433c08406dde1c
yankedev <yankedev@exteso.com>
2016-12-01 22:38:30 +0100

    add some domain models about future ReservationEngine

--- src/test/java/alfiov2/Domain.java
+++ src/test/java/alfiov2/Domain.java
@@ -1,12 +1,25 @@
+/**
+ * This file is part of alf.io.
+ *
+ * alf.io is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * alf.io is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package alfiov2;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by yanke on 01.12.16.
- */
 public class Domain {
 
     static class Event {
