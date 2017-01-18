11716514eb52d258061c6bf6be00ffffb71ed03e
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-01-18 23:49:43 +0100

    #219 fix mysql...

--- src/main/resources/alfio/db/MYSQL/V18_1.10.0__ADD_INVOICE_SUPPORT.sql
+++ src/main/resources/alfio/db/MYSQL/V18_1.10.0__ADD_INVOICE_SUPPORT.sql
@@ -15,6 +15,28 @@
 -- along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
 --
 
+-- fix mysql horrible auto updating behaviour, see http://stackoverflow.com/a/31865524
+
+
+ALTER TABLE ticket CHANGE COLUMN  creation creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE ticket ALTER COLUMN creation DROP DEFAULT;
+
+ALTER TABLE tickets_reservation CHANGE COLUMN  validity validity TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE tickets_reservation ALTER COLUMN validity DROP DEFAULT;
+
+ALTER TABLE b_transaction CHANGE COLUMN  t_timestamp t_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE b_transaction ALTER COLUMN t_timestamp DROP DEFAULT;
+
+ALTER TABLE plugin_log CHANGE COLUMN  event_ts event_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE plugin_log ALTER COLUMN event_ts DROP DEFAULT;
+
+ALTER TABLE waiting_queue CHANGE COLUMN  creation creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE waiting_queue ALTER COLUMN creation DROP DEFAULT;
+
+ALTER TABLE sponsor_scan CHANGE COLUMN  creation creation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE sponsor_scan ALTER COLUMN creation DROP DEFAULT;
+--
+
 create table invoice_sequences (
     event_id_fk integer primary key not null,
     sequence integer not null
