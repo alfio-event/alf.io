c8b80a13a51370131e499fdd519f12c102e12e05
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-10-08 20:05:43 +0200

    should work now...

--- src/main/java/alfio/repository/SpecialPriceRepository.java
+++ src/main/java/alfio/repository/SpecialPriceRepository.java
@@ -84,10 +84,10 @@ public interface SpecialPriceRepository {
     @Query("update special_price set status = 'CANCELLED' where ticket_category_id = :categoryId and status in ('FREE', 'WAITING')")
     int cancelExpiredTokens(@Bind("categoryId") int categoryId);
 
-    @Query("select id from special_price where ticket_category_id = :categoryId and status in ('FREE', 'WAITING') and recipient_email is null limit :limit for update")
+    @Query("select id from special_price where ticket_category_id = :categoryId and status in ('FREE', 'WAITING') and sent_ts is null limit :limit for update")
     List<Integer> lockNotSentTokens(@Bind("categoryId") int categoryId, @Bind("limit") int limit);
 
-    @Query("select count(id) from special_price where ticket_category_id = :categoryId and status in ('FREE', 'WAITING') and recipient_email is null")
+    @Query("select count(id) from special_price where ticket_category_id = :categoryId and status in ('FREE', 'WAITING') and sent_ts is null")
     Integer countNotSentToken(@Bind("categoryId") int categoryId);
 
     @Query("update special_price set status = 'CANCELLED' where id in (:ids)")
