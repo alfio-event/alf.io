38d21133ee5da936f2bc06d9c6fb9e721f4827b2
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-03-06 15:07:59 +0100

    #349 load extension data in config panel

--- src/main/webapp/resources/js/admin/feature/extension/extension.service.js
+++ src/main/webapp/resources/js/admin/feature/extension/extension.service.js
@@ -4,7 +4,7 @@
     angular.module('adminApplication').service('ExtensionService', ExtensionService);
 
 
-    function ExtensionService($http, HttpErrorHandler) {
+    function ExtensionService($http, HttpErrorHandler, EventService, OrganizationService, $q) {
         return {
             loadEventConfig: function(organizationName, eventShortName) {
                 return $http.get('/admin/api/extensions/setting/organization/'+organizationName+'/event/'+eventShortName).error(HttpErrorHandler.handle);
@@ -14,11 +14,23 @@
             },
             loadSystem: function() {
                 return $http.get('/admin/api/extensions/setting/system').error(HttpErrorHandler.handle);
+            },
+            loadEventConfigWithOrgIdAndEventId: function(orgId, eventId) {
+                var service = this;
+                return $q.all([OrganizationService.getOrganization(orgId), EventService.getEventById(eventId)]).then(function(res) {
+                    return service.loadEventConfig(res[0].data.name, res[1].data.shortName);
+                })
+            },
+            loadOrganizationConfigWithOrgId: function (orgId) {
+                var service = this;
+                return OrganizationService.getOrganization(orgId).then(function(org) {
+                    return service.loadOrganizationConfig(org.data.name);
+                });
             }
         };
     }
 
-    ExtensionService.$inject = ['$http', 'HttpErrorHandler'];
+    ExtensionService.$inject = ['$http', 'HttpErrorHandler', 'EventService', 'OrganizationService', '$q'];
 
 
 })();
