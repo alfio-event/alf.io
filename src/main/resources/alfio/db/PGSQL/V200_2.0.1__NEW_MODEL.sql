a9e151b5b4d251c2d966a5940c1e55423b4f36db
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-04-19 13:52:48 +0200

    additional work in the new model: fk, new table+complete the pgsql tasks

--- src/main/resources/alfio/db/PGSQL/V200_2.0.1__NEW_MODEL.sql
+++ src/main/resources/alfio/db/PGSQL/V200_2.0.1__NEW_MODEL.sql
@@ -27,6 +27,12 @@ create table v2_event (
     end_time timestamp
 );
 
+create table v2_resource (
+    id bigint not null primary key,
+    name varchar(256),
+    resource_id_fk bigint
+);
+alter table v2_resource add constraint "v2_resource_self_fk" foreign key(resource_id_fk) references v2_resource(id);
 
 create table v2_event_resource (
     id bigint not null primary key,
@@ -35,6 +41,8 @@ create table v2_event_resource (
     start_time timestamp,
     end_time timestamp
 );
+alter table v2_event_resource add constraint "v2_event_resource_event_id_fk" foreign key(event_id_fk) references v2_event(id);
+alter table v2_event_resource add constraint "v2_event_resource_resource_id_fk" foreign key(resource_id_fk) references v2_resource(id);
 
 create type slot_creation_strategy_t as enum ('EAGER', 'LAZY');
 
@@ -44,10 +52,11 @@ create table v2_ticket_category (
     start_time timestamp,
     end_time timestamp,
     max_tickets int,
-    name varchar(128) not null,
+    name varchar(256) not null,
     src_price_cts int,
     slot_creation_strategy slot_creation_strategy_t
 );
+alter table v2_ticket_category add constraint "v2_ticket_category_event_resource_id_fk" foreign key(event_resource_id_fk) references v2_event_resource(id);
 
 create table v2_slot (
     id bigint not null primary key,
@@ -57,23 +66,30 @@ create table v2_slot (
     end_time timestamp,
     ticket_id_fk bigint
 );
+alter table v2_slot add constraint "v2_slot_event_resource_id_fk" foreign key(event_resource_id_fk) references v2_event_resource(id);
 
 -- https://stackoverflow.com/questions/26735955/postgres-constraint-for-unique-datetime-range
 -- TODO: check that setting exclusive use to null OR start_time/end_time to null will deactivate this constraint :D
-alter table v2_slot add constraint unique_overlapping_time exclude using gist (
+alter table v2_slot add constraint "v2_slot_unique_overlapping_time" exclude using gist (
     event_resource_id_fk WITH =,
     (exclusive_use::int) WITH =, -- <- boolean is not supported
     tsrange(start_time, end_time) WITH &&
 );
 
+create table v2_reservation (
+    id bigint not null primary key,
+    uuid uuid,
+    event_id_fk bigint
+);
+alter table v2_reservation add constraint "v2_reservation_event_id_fk" foreign key(event_id_fk) references v2_event(id);
+
 create table v2_ticket (
     id bigint not null primary key,
     uuid uuid,
     creation_time timestamp,
-    category_id_fk bigint
+    ticket_category_id_fk bigint,
+    reservation_id_fk bigint
 );
+alter table v2_ticket add constraint "v2_ticket_ticket_category_id_fk" foreign key(ticket_category_id_fk) references v2_ticket_category(id);
+alter table v2_ticket add constraint "v2_ticket_reservation_id_fk" foreign key(reservation_id_fk) references v2_reservation(id);
 
-create table v2_reservation (
-    id bigint not null primary key,
-    uuid uuid
-);
