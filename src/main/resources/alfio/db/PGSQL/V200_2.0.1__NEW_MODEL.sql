f947089c6cb400376ee12d9c966a200cf86c7086
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-04-20 14:58:40 +0200

    new model: switch to serial for auto incrementing id

--- src/main/resources/alfio/db/PGSQL/V200_2.0.1__NEW_MODEL.sql
+++ src/main/resources/alfio/db/PGSQL/V200_2.0.1__NEW_MODEL.sql
@@ -21,21 +21,24 @@
 create extension btree_gist;
 
 create table v2_event (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     short_name varchar(128) not null,
     start_time timestamp,
-    end_time timestamp
+    end_time timestamp,
+    time_zone varchar(255)
 );
 
+alter table v2_event add constraint "v2_event_unique_short_name" unique(short_name);
+
 create table v2_resource (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     name varchar(256),
     resource_id_fk bigint
 );
 alter table v2_resource add constraint "v2_resource_self_fk" foreign key(resource_id_fk) references v2_resource(id);
 
 create table v2_event_resource (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     event_id_fk bigint not null,
     resource_id_fk bigint not null,
     start_time timestamp,
@@ -47,7 +50,7 @@ alter table v2_event_resource add constraint "v2_event_resource_resource_id_fk" 
 create type slot_creation_strategy_t as enum ('EAGER', 'LAZY');
 
 create table v2_ticket_category (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     event_resource_id_fk bigint not null,
     start_time timestamp,
     end_time timestamp,
@@ -59,7 +62,7 @@ create table v2_ticket_category (
 alter table v2_ticket_category add constraint "v2_ticket_category_event_resource_id_fk" foreign key(event_resource_id_fk) references v2_event_resource(id);
 
 create table v2_slot (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     event_resource_id_fk bigint,
     exclusive_use boolean,
     start_time timestamp,
@@ -77,14 +80,14 @@ alter table v2_slot add constraint "v2_slot_unique_overlapping_time" exclude usi
 );
 
 create table v2_reservation (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     uuid uuid,
     event_id_fk bigint
 );
 alter table v2_reservation add constraint "v2_reservation_event_id_fk" foreign key(event_id_fk) references v2_event(id);
 
 create table v2_ticket (
-    id bigint not null primary key,
+    id bigserial not null primary key,
     uuid uuid,
     creation_time timestamp,
     ticket_category_id_fk bigint,
