f7dc5cc4d6834a3e14814b540447956d1145b4e5
Celestino Bellone <celestino@swicket.io>
2019-05-17 18:03:30 +0200

    add first tutorial for SCA

--- docs/index.md
+++ docs/index.md
@@ -2,4 +2,5 @@
 
 #### How to...
 
-- [Configure Apple(tm) Pass integration](howto/configuration/apple-pass/pass.md)
+- [Configure Apple(tm) Pass integration](howto/configuration/apple-pass/index.md)
+- [Configure Stripe for Strong Customer Authentication](howto/configuration/stripe-sca/index.md)
--- src/main/java/alfio/model/system/ConfigurationKeys.java
+++ src/main/java/alfio/model/system/ConfigurationKeys.java
@@ -62,7 +62,7 @@ public enum ConfigurationKeys {
     STRIPE_CONNECT_CLIENT_ID("Stripe Connect Client ID", false, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.of(SYSTEM)),
     STRIPE_CONNECT_CALLBACK("Stripe Connect Callback URL", false, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.of(SYSTEM)),
     STRIPE_WEBHOOK_KEY("Stripe Signature key for Account-related events", false, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.of(SYSTEM)),
-    STRIPE_WEBHOOK_PAYMENT_KEY("Stripe Payment Webhook signature key", false, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.of(SYSTEM)),
+    STRIPE_WEBHOOK_PAYMENT_KEY("Payment Webhook signing secret", false, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.of(SYSTEM)),
     STRIPE_CONNECTED_ID("Connected ID", true, SettingCategory.PAYMENT_STRIPE, ComponentType.TEXT, false, EnumSet.noneOf(ConfigurationPathLevel.class)),
     STRIPE_ENABLE_SCA("Enable Strong Customer Authentication", false, SettingCategory.PAYMENT_STRIPE, ComponentType.BOOLEAN, false, EnumSet.of(SYSTEM)),
 
