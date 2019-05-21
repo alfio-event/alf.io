d98677953230c2b0f81f6e5eb82a523e15795441
Celestino Bellone <celestino@swicket.io>
2019-05-21 22:21:11 +0200

    #462 - remove unused modules

--- src/main/webapp/resources/js/admin/ng-app/admin-application.js
+++ src/main/webapp/resources/js/admin/ng-app/admin-application.js
@@ -10,7 +10,7 @@
     
     var admin = angular.module('adminApplication', ['ngSanitize','ui.bootstrap', 'ui.router', 'adminDirectives',
         'adminServices', 'utilFilters', 'ngMessages', 'ngFileUpload', 'nzToggle', 'alfio-email', 'alfio-util', 'alfio-configuration', 'alfio-additional-services', 'alfio-event-statistic',
-        'ui.ace', 'monospaced.qrcode', 'checklist-model', 'group', 'datatables', 'datatables.bootstrap']);
+        'ui.ace', 'monospaced.qrcode', 'checklist-model', 'group']);
 
     var loadEvent = {
         'loadEvent': function($stateParams, EventService) {
