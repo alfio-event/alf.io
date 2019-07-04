a2ad1264869b423cb563aa2697dc9ef67e840a31
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-07-04 16:01:31 +0200

    add new key for translation override #439

--- src/main/java/alfio/model/system/ConfigurationKeys.java
+++ src/main/java/alfio/model/system/ConfigurationKeys.java
@@ -213,7 +213,10 @@ public enum ConfigurationKeys {
     TRANSLATION_OVERRIDE_VAT_NL("Override the default tax term NL: BTW", false, SettingCategory.TRANSLATIONS, ComponentType.TEXT, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT)),
     TRANSLATION_OVERRIDE_VAT_RO("Override the default tax term RO: TVA", false, SettingCategory.TRANSLATIONS, ComponentType.TEXT, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT)),
     TRANSLATION_OVERRIDE_VAT_PT("Override the default tax term PT: IVA", false, SettingCategory.TRANSLATIONS, ComponentType.TEXT, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT)),
-    TRANSLATION_OVERRIDE_VAT_TR("Override the default tax term TR: KDV", false, SettingCategory.TRANSLATIONS, ComponentType.TEXT, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT));
+    TRANSLATION_OVERRIDE_VAT_TR("Override the default tax term TR: KDV", false, SettingCategory.TRANSLATIONS, ComponentType.TEXT, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT)),
+
+    //
+    TRANSLATION_OVERRIDE("Translation override (json)", true, SettingCategory.TRANSLATIONS, ComponentType.TEXTAREA, false, EnumSet.of(SYSTEM, ORGANIZATION, EVENT));
 
     @Getter
     public enum SettingCategory {
