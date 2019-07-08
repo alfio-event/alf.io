9d5cfcc7dea6c1847e3080a450a6c76d4a79a264
Celestino Bellone <celestino@swicket.io>
2019-07-08 21:25:07 +0200

    #684 - find reservations by clicking on promo code

--- src/main/webapp/resources/js/admin/ng-app/admin-application.js
+++ src/main/webapp/resources/js/admin/ng-app/admin-application.js
@@ -222,7 +222,7 @@
                 }
             })
             .state('events.single.reservationsList', {
-                url: '/reservations/',
+                url: '/reservations/?search',
                 template: '<reservations-list event="ctrl.event"></reservations-list>',
                 controller: function(getEvent) {
                     this.event = getEvent.data.event;
