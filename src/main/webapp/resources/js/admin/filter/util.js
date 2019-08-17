42e2a78ded6226659e25feced2e8c31f9af12e3c
Celestino Bellone <celestino@swicket.io>
2019-08-17 18:58:36 +0200

    consider that currency can also be at the end of the string

--- src/main/webapp/resources/js/admin/filter/util.js
+++ src/main/webapp/resources/js/admin/filter/util.js
@@ -147,7 +147,8 @@
             }
             var formatted = new Intl.NumberFormat(navigator.language, { style: 'currency', currency: currency, currencyDisplay: 'code' }).format(amount);
             if(hideCurrency) {
-                return formatted.substring(4);
+                var currencyIndex = formatted.indexOf(currency);
+                return (formatted.substring(0, currencyIndex) + formatted.substring(currencyIndex + currency.length)).trim();
             }
             return formatted;
         }
