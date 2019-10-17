cb6183893c4b9b7ea25a89b733b285398f72a479
Celestino Bellone <celestino@swicket.io>
2019-10-17 15:16:40 +0200

    update languages layout: remove flag, as it can be misleading

--- src/main/java/alfio/model/ContentLanguage.java
+++ src/main/java/alfio/model/ContentLanguage.java
@@ -27,16 +27,16 @@ import java.util.stream.Collectors;
 public class ContentLanguage {
 
     public static final int ENGLISH_IDENTIFIER = 0b00010;
-    public static final ContentLanguage ITALIAN = new ContentLanguage(Locale.ITALIAN, 0b00001, Locale.ITALIAN, "it");
-    public static final ContentLanguage ENGLISH = new ContentLanguage(Locale.ENGLISH, ENGLISH_IDENTIFIER, Locale.ENGLISH, "gb");
-    public static final ContentLanguage GERMAN = new ContentLanguage(Locale.GERMAN,   0b00100, Locale.GERMAN, "de");
-    public static final ContentLanguage DUTCH = new ContentLanguage(LocaleUtil.forLanguageTag("nl"), 0b01000, LocaleUtil.forLanguageTag("nl"), "nl");
-    public static final ContentLanguage FRENCH = new ContentLanguage(Locale.FRENCH,0b10000, Locale.FRENCH, "fr");
-    public static final ContentLanguage ROMANIAN = new ContentLanguage(LocaleUtil.forLanguageTag("ro"),0b100000, LocaleUtil.forLanguageTag("ro"), "ro");
-    public static final ContentLanguage PORTUGUESE = new ContentLanguage(LocaleUtil.forLanguageTag("pt"),0b1000000, LocaleUtil.forLanguageTag("pt"), "pt");
-    public static final ContentLanguage TURKISH = new ContentLanguage(LocaleUtil.forLanguageTag("tr"),0b10000000, LocaleUtil.forLanguageTag("tr"), "tr");
-    public static final ContentLanguage SPANISH = new ContentLanguage(LocaleUtil.forLanguageTag("es"),0b100000000, LocaleUtil.forLanguageTag("es"), "es");
-    public static final ContentLanguage POLISH = new ContentLanguage(LocaleUtil.forLanguageTag("pl"),0b1000000000, LocaleUtil.forLanguageTag("pl"), "pl");
+    public static final ContentLanguage ITALIAN = new ContentLanguage(Locale.ITALIAN, 0b00001, Locale.ITALIAN);
+    public static final ContentLanguage ENGLISH = new ContentLanguage(Locale.ENGLISH, ENGLISH_IDENTIFIER, Locale.ENGLISH);
+    public static final ContentLanguage GERMAN = new ContentLanguage(Locale.GERMAN,   0b00100, Locale.GERMAN);
+    public static final ContentLanguage DUTCH = new ContentLanguage(LocaleUtil.forLanguageTag("nl"), 0b01000, LocaleUtil.forLanguageTag("nl"));
+    public static final ContentLanguage FRENCH = new ContentLanguage(Locale.FRENCH,0b10000, Locale.FRENCH);
+    public static final ContentLanguage ROMANIAN = new ContentLanguage(LocaleUtil.forLanguageTag("ro"),0b100000, LocaleUtil.forLanguageTag("ro"));
+    public static final ContentLanguage PORTUGUESE = new ContentLanguage(LocaleUtil.forLanguageTag("pt"),0b1000000, LocaleUtil.forLanguageTag("pt"));
+    public static final ContentLanguage TURKISH = new ContentLanguage(LocaleUtil.forLanguageTag("tr"),0b10000000, LocaleUtil.forLanguageTag("tr"));
+    public static final ContentLanguage SPANISH = new ContentLanguage(LocaleUtil.forLanguageTag("es"),0b100000000, LocaleUtil.forLanguageTag("es"));
+    public static final ContentLanguage POLISH = new ContentLanguage(LocaleUtil.forLanguageTag("pl"),0b1000000000, LocaleUtil.forLanguageTag("pl"));
 
     public static final List<ContentLanguage> ALL_LANGUAGES = List.of(ENGLISH, SPANISH, ITALIAN, GERMAN, DUTCH, FRENCH, ROMANIAN, PORTUGUESE, TURKISH, POLISH);
     public static final int ALL_LANGUAGES_IDENTIFIER = ALL_LANGUAGES.stream().mapToInt(ContentLanguage::getValue).reduce(0, (a,b) -> a|b);
@@ -50,13 +50,11 @@ public class ContentLanguage {
     private final Locale locale;
     private final int value;
     private final Locale displayLocale;
-    private final String flag;
 
-    private ContentLanguage(Locale locale, int value, Locale displayLocale, String flag) {
+    private ContentLanguage(Locale locale, int value, Locale displayLocale) {
         this.locale = locale;
         this.value = value;
         this.displayLocale = displayLocale;
-        this.flag = flag;
     }
 
     public String getLanguage() {
@@ -70,8 +68,6 @@ public class ContentLanguage {
     public Locale getLocale() {
         return locale;
     }
-
-    public String getFlag() { return flag; }
 
     public int getValue() {
         return value;
