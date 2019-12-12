8df8d9bd6aab96c60799aad3bc2923fb3846c4cd
Sylvain Jermini <sylvain.jermini@syjer.com>
2019-12-12 18:42:50 +0100

    configuration manager: fix parameter order

--- src/main/java/alfio/manager/system/ConfigurationManager.java
+++ src/main/java/alfio/manager/system/ConfigurationManager.java
@@ -284,7 +284,7 @@ public class ConfigurationManager {
             return null;
         }
         var key = safeValueOf(keyAsString);
-        return configurationRepository.findByEventAndKey(eventId, organizationId, key.name())
+        return configurationRepository.findByEventAndKey(organizationId, eventId, key.name())
             .stream()
             .findFirst()
             .map(Configuration::getValue)
