44a9b300c962ad5998e979e5886bcd9c124b8873
Celestino Bellone <celestino@swicket.io>
2020-01-09 11:49:38 +0100

    add test for checking date pattern in translations
    (cherry picked from commit 6ad56f3b1e93ccd71f4315ba0383242282df7e8f)

---
+++ src/test/java/alfio/controller/support/FormattersTest.java
@@ -0,0 +1,48 @@
+/**
+ * This file is part of alf.io.
+ *
+ * alf.io is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * alf.io is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package alfio.controller.support;
+
+import alfio.model.ContentLanguage;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * This test checks if all the date patterns for all defined languages are correct, in order to spot errors at build
+ * time.
+ */
+class FormattersTest {
+
+    private static final List<String> FORMATTER_CODES = List.of(
+        "common.event.date-format",
+        "datetime.pattern",
+        "common.ticket-category.date-format"
+    );
+
+    @Test
+    void getFormattedDates() {
+        var messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("alfio.i18n.public");
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+        ContentLanguage.ALL_LANGUAGES.forEach(cl ->
+            FORMATTER_CODES.forEach(code -> Formatters.formatDateForLocale(now, code, messageSource, (a, b) -> {}, cl, true))
+        );
+    }
+}
