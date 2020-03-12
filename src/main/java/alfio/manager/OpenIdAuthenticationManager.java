859a404ed39fc2709b9356338f4d1edafe72e2c0
Matteo Bresciani <matteobresciani@Matteos-MacBook-Pro.local>
2020-03-12 16:01:53 +0100

    refactor of OpenId classes

--- src/main/java/alfio/manager/OpenIdAuthenticationManager.java
+++ src/main/java/alfio/manager/OpenIdAuthenticationManager.java
@@ -1,18 +1,141 @@
 package alfio.manager;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+    import com.fasterxml.jackson.core.JsonProcessingException;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+    import org.springframework.beans.factory.annotation.*;
+    import org.springframework.context.annotation.Profile;
+    import org.springframework.stereotype.Component;
 
-import java.util.*;
+    import java.net.URI;
+    import java.net.http.HttpRequest;
+    import java.util.*;
 
-public interface OpenIdAuthenticationManager
+@Component
+@Profile("oauth2")
+public class OpenIdAuthenticationManager
 {
-    String buildAuthorizeUrl(List<String> scopes);
-    String buildClaimsRetrieverUrl();
-    String buildRetrieveClaimsUrlBody(String code) throws JsonProcessingException;
+    private final String domain;
+    private final String clientId;
+    private final String clientSecret;
+    private final String callbackURI;
+    private final String authenticationUrl;
+    private final String claimsUrl;
+    private String grantType;
+    private String contentType;
 
-    String getCodeNameParameter();
-    String getAccessTokenNameParameter();
-    String getIdTokenNameParameter();
-    String getSubjectNameParameter();
-    String getEmailNameParameter();
+    public OpenIdAuthenticationManager(@Value("${oauth2.domain}") String domain,
+                                      @Value("${oauth2.clientId}") String clientId,
+                                      @Value("${oauth2.clientSecret}") String clientSecret,
+                                      @Value("${oauth2.callbackURI}") String callbackURI,
+                                      @Value("${oauth2.authenticationUrl}") String authenticationUrl,
+                                      @Value("${oauth2.claimsUrl}") String claimsUrl,
+                                      @Value("${oauth2.grantType}") String grantType,
+                                      @Value("${oauth2.contentType}") String contentType){
+
+        this.domain = domain;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.callbackURI = callbackURI;
+        this.authenticationUrl = authenticationUrl;
+        this.claimsUrl = claimsUrl;
+        this.grantType = grantType;
+        this.contentType = contentType;
+    }
+
+    public String buildAuthorizeUrl(List<String> scopes){
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://");
+        builder.append(domain);
+        builder.append(authenticationUrl);
+        builder.append("?redirect_uri=");
+        builder.append(callbackURI);
+        builder.append("&client_id=");
+        builder.append(clientId);
+        builder.append("&scope=");
+
+        for(int i = 0; i < scopes.size(); i++){
+            if(i != 0)
+                builder.append("%20");
+            builder.append(scopes.get(i));
+        }
+
+        builder.append("&response_type=code");
+        return builder.toString();
+    }
+
+    public String buildClaimsRetrieverUrl()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://");
+        builder.append(domain);
+        builder.append(claimsUrl);
+        return builder.toString();
+    }
+
+    public String buildRetrieveClaimsUrlBody(String code) throws JsonProcessingException
+    {
+        if(contentType.equals("application/json"))
+            return buildRetrieveClaimsUrlJsonBody(code);
+        if(contentType.equals("application/x-www-form-urlencoded"))
+            return buildRetrieveClaimsUrlFormUrlEncodedBody(code);
+        throw new RuntimeException("the Content-Type you specifier is not supported");
+    }
+
+    public String buildRetrieveClaimsUrlJsonBody(String code) throws JsonProcessingException
+    {
+        Map<String, String> body = new HashMap<String, String>() {{
+            put("grant_type", grantType);
+            put("code", code);
+            put("client_id", clientId);
+            put("client_secret", clientSecret);
+            put("redirect_uri", callbackURI);
+        }};
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return objectMapper.writeValueAsString(body);
+    }
+
+    public String buildRetrieveClaimsUrlFormUrlEncodedBody(String code)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("grant_type=" + grantType);
+        stringBuilder.append("&code=" + code);
+        stringBuilder.append("&client_id=" + clientId);
+        stringBuilder.append("&client_secret=" + clientSecret);
+        stringBuilder.append("&redirect_uri=" + callbackURI);
+
+        return stringBuilder.toString();
+    }
+
+    public String getCodeNameParameter()
+    {
+        return "code";
+    }
+
+    public String getAccessTokenNameParameter()
+    {
+        return "access_token";
+    }
+
+    public String getIdTokenNameParameter()
+    {
+        return "id_token";
+    }
+
+    public String getSubjectNameParameter()
+    {
+        return "sub";
+    }
+
+    public String getEmailNameParameter()
+    {
+        return "email";
+    }
+
+    public String getContentType()
+    {
+        return contentType;
+    }
 }
+
--- src/test/java/alfio/manager/OpenIdAuthenticationManagerTest.java
+++ src/test/java/alfio/manager/OpenIdAuthenticationManagerTest.java
@@ -1,33 +1,33 @@
 package alfio.manager;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.*;
+    import com.fasterxml.jackson.core.JsonProcessingException;
+    import org.junit.*;
 
-import java.util.*;
+    import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class GoogleAuthenticationManagerTest
+public class OpenIdAuthenticationManagerTest
 {
     private final String DOMAIN = "domain_test";
     private final String CLIENT_ID = "123";
     private final String CLIENT_SECRET = "1234";
     private final String CALLBACK_URI = "callback";
+    private final String AUTHENTICATION_URL = "/auth";
     private final String CLAIMS_URI = "/claims";
+    private final String CONTENT_TYPE = "application/json";
 
-    private final GoogleAuthenticationManager authenticationManager = new GoogleAuthenticationManager(DOMAIN, CLIENT_ID, CLIENT_SECRET, CALLBACK_URI, CLAIMS_URI);
+    private final OpenIdAuthenticationManager authenticationManager = new OpenIdAuthenticationManager(DOMAIN, CLIENT_ID, CLIENT_SECRET, CALLBACK_URI, AUTHENTICATION_URL, CLAIMS_URI, "authorization_code", CONTENT_TYPE);
 
     @Test
-    public void google_authorize_url_test()
+    public void oauth2_authorize_url_test()
     {
         List<String> scopes = Arrays.asList("scope1", "scope2");
         String redirectURL = authenticationManager.buildAuthorizeUrl(scopes);
-        String expectedURL = "https://domain_test/o/oauth2/v2/auth?redirect_uri=callback&client_id=123&scope=scope1%20scope2&response_type=code";
+        String expectedURL = "https://domain_test/auth?redirect_uri=callback&client_id=123&scope=scope1%20scope2&response_type=code";
         Assert.assertEquals(expectedURL, redirectURL);
     }
 
     @Test
-    public void google_claims_url_test()
+    public void oauth2_claims_url_test()
     {
         String claimsUrl = authenticationManager.buildClaimsRetrieverUrl();
         String expectedURL = "https://domain_test/claims";
@@ -35,19 +35,20 @@ public class GoogleAuthenticationManagerTest
     }
 
     @Test
-    public void google_build_body_test() throws JsonProcessingException
+    public void oauth2_build_body_test() throws JsonProcessingException
     {
         String code = "code";
-        String body = authenticationManager.buildRetrieveClaimsUrlBody(code);
+        String body = authenticationManager.buildRetrieveClaimsUrlJsonBody(code);
         String expectedBody = "{\"code\":\"code\",\"grant_type\":\"authorization_code\",\"client_secret\":\"1234\",\"redirect_uri\":\"callback\",\"client_id\":\"123\"}";
         Assert.assertEquals(expectedBody, body);
     }
 
     @Test
-    public void google_parameters_name_test(){
+    public void oauth2_parameters_name_test(){
         Assert.assertEquals("code", authenticationManager.getCodeNameParameter());
         Assert.assertEquals("access_token", authenticationManager.getAccessTokenNameParameter());
         Assert.assertEquals("id_token", authenticationManager.getIdTokenNameParameter());
         Assert.assertEquals("sub", authenticationManager.getSubjectNameParameter());
     }
+
 }
