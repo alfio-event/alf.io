7b38baec28b7e2a38981ffda1407c0b0d5fb446e
Celestino Bellone <celestino@swicket.io>
2021-05-23 11:33:13 +0200

    add short reservationId to the BillingDocument context

--- src/main/java/alfio/manager/BillingDocumentManager.java
+++ src/main/java/alfio/manager/BillingDocumentManager.java
@@ -173,7 +173,8 @@ public class BillingDocumentManager {
         } else {
             ticketsWithCategory = Collections.emptyList();
         }
-        Map<String, Object> model = TemplateResource.prepareModelForConfirmationEmail(organization, purchaseContext, reservation, vat, ticketsWithCategory, summary, "", "", "", invoiceAddress, bankAccountNr, bankAccountOwner, Map.of());
+        var reservationShortId = configurationManager.getShortReservationID(purchaseContext, reservation);
+        Map<String, Object> model = TemplateResource.prepareModelForConfirmationEmail(organization, purchaseContext, reservation, vat, ticketsWithCategory, summary, "", "", reservationShortId, invoiceAddress, bankAccountNr, bankAccountOwner, Map.of());
         boolean euBusiness = StringUtils.isNotBlank(reservation.getVatCountryCode()) && StringUtils.isNotBlank(reservation.getVatNr())
             && configurationManager.getForSystem(ConfigurationKeys.EU_COUNTRIES_LIST).getRequiredValue().contains(reservation.getVatCountryCode())
             && PriceContainer.VatStatus.isVatExempt(reservation.getVatStatus());
