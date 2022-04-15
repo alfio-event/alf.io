941e3a1613a3314b408f71b601cdcf23ee8c0b62
Sylvain Jermini <sylvain.jermini@syjer.com>
2022-04-15 15:45:36 +0200

    add test for checking if we are able to deserialize json -> java object in our RequestBody parameters

--- src/test/java/alfio/controller/api/CheckJsonBodyRequest.java
+++ src/test/java/alfio/controller/api/CheckJsonBodyRequest.java
@@ -1,5 +1,20 @@
+/**
+ * This file is part of alf.io.
+ *
+ * alf.io is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * alf.io is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package alfio.controller.api;
-
 
 import alfio.TestConfiguration;
 import alfio.config.DataSourceConfiguration;
@@ -14,7 +29,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import javax.lang.model.type.ArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.util.*;
 
