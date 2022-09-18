c5d7159c9b5f32b09fc1a63cd640cb2b6115953e
Sylvain Jermini <sylvain.jermini@syjer.com>
2022-09-18 16:23:16 +0200

    do a right outer join in event statistics view to handle the case of an event without ticket category (note, this is possible only if done by modifying directly the database), should handle #1103 (#1113)
    (cherry picked from commit 39829281a3187f390b295a1853d5a29b94e66695)

--- src/main/resources/alfio/db/PGSQL/afterMigrate__004_VIEW_events_statistics.sql
+++ src/main/resources/alfio/db/PGSQL/afterMigrate__004_VIEW_events_statistics.sql
@@ -15,15 +15,16 @@
 -- along with alf.io.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-create view events_statistics as (select
+create view events_statistics as (
+select
       event.id,
       (select count(id) from ticket where event_id = event.id and status not in ('INVALIDATED', 'EXPIRED')) as available_seats,
-      case(contains_unbounded_categories) when true then 0 else (select count(id) from ticket where event_id = event.id and status not in ('INVALIDATED', 'EXPIRED')) - allocated_count end as not_allocated_tickets,
-      pending_count as pending_tickets,
-      sold_tickets_count as sold_tickets,
-      (select released_count + count(id) from ticket where event_id = event.id and status = 'RELEASED' and category_id is null) as released_tickets,
-      stats.checked_in_count as checked_in_tickets,
-      case(contains_unbounded_categories) when true then
+      coalesce( case(contains_unbounded_categories) when true then 0 else (select count(id) from ticket where event_id = event.id and status not in ('INVALIDATED', 'EXPIRED')) - allocated_count end, 0) as not_allocated_tickets,
+      coalesce (pending_count, 0) as pending_tickets,
+      coalesce (sold_tickets_count, 0) as sold_tickets,
+      coalesce ( (select released_count + count(id) from ticket where event_id = event.id and status = 'RELEASED' and category_id is null),0) as released_tickets,
+      coalesce(stats.checked_in_count, 0) as checked_in_tickets,
+      coalesce (case(contains_unbounded_categories) when true then
         (select count(id) from ticket where event_id = event.id and status not in ('INVALIDATED', 'EXPIRED'))
           - allocated_count
           - released_count
@@ -31,18 +32,18 @@ create view events_statistics as (select
           - checked_in_count_unbounded
           - pending_count_unbounded
           - (select count(*) from ticket where status = 'RELEASED' and category_id is null and event_id = event.id)
-          else 0 end as dynamic_allocation,
-      case (contains_unbounded_categories) when true then
-        allocated_count - sold_tickets_count_bounded - checked_in_count_bounded - pending_count_bounded
-      else
-        allocated_count - sold_tickets_count - stats.checked_in_count - pending_count
-      end as not_sold_tickets,
-      is_containing_orphan_tickets_count > 0 as is_containing_orphan_tickets,
-      is_containing_stuck_tickets_count > 0 as is_containing_stuck_tickets_count,
-      public_and_valid_count > 0 as show_public_statistics
-
-from
-(select
+          else 0 end, 0) as dynamic_allocation,
+      coalesce (
+      	case (contains_unbounded_categories) when true then
+        	allocated_count - sold_tickets_count_bounded - checked_in_count_bounded - pending_count_bounded
+      	else
+        	allocated_count - sold_tickets_count - stats.checked_in_count - pending_count
+      	end, 0) as not_sold_tickets,
+      coalesce (is_containing_orphan_tickets_count > 0, false) as is_containing_orphan_tickets,
+      coalesce (is_containing_stuck_tickets_count > 0, false) as is_containing_stuck_tickets_count,
+      coalesce (public_and_valid_count > 0, false) as show_public_statistics
+from (
+select
 	sum(sold_tickets_count) as sold_tickets_count,
 	sum(checked_in_count) as checked_in_count,
 	sum(pending_count) as pending_count,
@@ -59,4 +60,5 @@ from
     sum(case (is_containing_stuck_tickets) when true then 1 else 0 end) is_containing_stuck_tickets_count,
     sum(case (access_restricted = false and is_expired = false) when true then 1 else 0 end) as public_and_valid_count,
 	event_id from ticket_category_statistics group by event_id) as stats
-inner join event on event_id = event.id order by event.start_ts, event.end_ts);
+right outer join event on event_id = event.id order by event.start_ts, event.end_ts
+);
