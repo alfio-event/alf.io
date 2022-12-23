197051988f9f45135488ca4784c5225716baa4f3
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2022-12-23 13:17:46 +0100

    first level menu

--- frontend-admin/src/app/shared/icons.ts
+++ frontend-admin/src/app/shared/icons.ts
@@ -1,9 +1,13 @@
-import { addIcon } from '../svg/add';
-import { arrowdropdownIcon } from '../svg/arrowdropdown';
+import {addIcon} from '../svg/add';
+import {arrowdropdownIcon} from '../svg/arrowdropdown';
 import {checkIcon} from '../svg/check';
 import {homeIcon} from '../svg/home';
-import { organizationIcon } from '../svg/organization';
-import { settingsIcon } from '../svg/settings';
+import {organizationIcon} from '../svg/organization';
+import {settingsIcon} from '../svg/settings';
+import {eventIcon} from "../svg/event";
+import {accessIcon} from "../svg/access";
+import {groupsIcon} from "../svg/groups";
+import {subscriptionIcon} from "../svg/subscription";
 
 const ICONS = [
   checkIcon,
@@ -13,6 +17,10 @@ const ICONS = [
   organizationIcon,
   settingsIcon,
   arrowdropdownIcon,
+  eventIcon,
+  accessIcon,
+  groupsIcon,
+  subscriptionIcon
 ];
 export const ICON_CONFIG = {
   sizes: {
