33b24d3198a032212b7575d2d1361271b8252a84
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-04-27 14:47:44 +0200

    Functionality edit

--- frontend/projects/admin/src/app/organization-edit/organization-edit.component.ts
+++ frontend/projects/admin/src/app/organization-edit/organization-edit.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrganizationService } from '../shared/organization.service';
+import { Observable, map, of, switchMap } from 'rxjs';
+import { Organization } from '../model/organization';
 
 @Component({
   selector: 'app-organization-edit',
   templateUrl: './organization-edit.component.html',
-  styleUrls: ['./organization-edit.component.scss']
+  styleUrls: ['./organization-edit.component.scss'],
 })
 export class OrganizationEditComponent implements OnInit {
+  public organizationId$: Observable<string | null> = of();
+  public organization$: Observable<Organization | null> = of();
 
-  constructor() { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly oganizationService: OrganizationService
+  ) {}
 
   ngOnInit(): void {
+    this.organizationId$ = this.route.paramMap.pipe(
+      map((pm) => pm.get('organizationId'))
+    );
+    this.organization$ = this.organizationId$.pipe(
+      switchMap((v) => (v != null ? this.oganizationService.getOrganization(v) : of()))
+    );
   }
-
 }
--- frontend/projects/admin/src/app/shared/organization.service.ts
+++ frontend/projects/admin/src/app/shared/organization.service.ts
@@ -1,14 +1,17 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
-import {Organization} from "../model/organization";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Organization } from '../model/organization';
 
 @Injectable()
 export class OrganizationService {
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   getOrganizations(): Observable<Organization[]> {
     return this.httpClient.get<Organization[]>('/admin/api/organizations');
+  }
+
+  getOrganization(id: number | string): Observable<Organization> {
+    return this.httpClient.get<Organization>(`/admin/api/organizations/${id}`);
   }
 }
