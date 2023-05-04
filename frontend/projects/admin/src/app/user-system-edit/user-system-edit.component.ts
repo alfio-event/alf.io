888a600180627a79857d24b2abf541cdfe60d67d
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-05-04 16:36:53 +0200

    Create form users new

--- frontend/projects/admin/src/app/user-system-edit/user-system-edit.component.ts
+++ frontend/projects/admin/src/app/user-system-edit/user-system-edit.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-system-edit',
   templateUrl: './user-system-edit.component.html',
-  styleUrls: ['./user-system-edit.component.scss']
+  styleUrls: ['./user-system-edit.component.scss'],
 })
 export class UserSystemEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public userForm: FormGroup;
+  constructor(formBuilder: FormBuilder) {
+    this.userForm = formBuilder.group({
+      target: null,
+      organizationId: [null, Validators.required],
+      role: [null, Validators.required],
+      username: [null, Validators.required],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      emailAddress: [null, Validators.required],
+    });
   }
 
+  get userName() {
+    return this.userForm.get('username');
+  }
+  get userLastName() {
+    return this.userForm.get('lastName');
+  }
+  get userFirstName() {
+    return this.userForm.get('firstName');
+  }
+
+  get userMail() {
+    return this.userForm.get('emailAddress');
+  }
+
+  ngOnInit(): void {}
+
+  save() : void {
+    console.log(this.userForm.value);
+  }
 }
