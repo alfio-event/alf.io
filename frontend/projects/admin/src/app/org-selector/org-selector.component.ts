73565d5eb336440efbfd8e4b65e15b97c6c187f1
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-05-05 16:19:17 +0200

    Remove console.log

--- frontend/projects/admin/src/app/org-selector/org-selector.component.ts
+++ frontend/projects/admin/src/app/org-selector/org-selector.component.ts
@@ -1,28 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {Observable} from 'rxjs';
-import {Organization} from '../model/organization';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { Organization } from '../model/organization';
 
 @Component({
   selector: 'app-org-selector',
   templateUrl: './org-selector.component.html',
-  styleUrls: ['./org-selector.component.scss']
+  styleUrls: ['./org-selector.component.scss'],
 })
-
-
 export class OrgSelectorComponent implements OnInit {
-
   public organizations$?: Observable<Organization[]>;
   public organizationId$?: Observable<string | null>;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public selectOrganization(org: Organization): void {
     this.activeModal.close(org);
-    console.log('selectOrg', org);
   }
-
 }
