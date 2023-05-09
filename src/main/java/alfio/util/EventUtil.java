d7e319c871db54a2cb975f1fd7a18cfb758d03c5
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2023-05-09 16:19:00 +0200

    fix reverse proxy error when description is too long. Truncate to 1024 chars

--- src/main/java/alfio/util/EventUtil.java
+++ src/main/java/alfio/util/EventUtil.java
@@ -37,6 +37,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.RegExUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.flywaydb.core.api.MigrationVersion;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -211,7 +212,7 @@ public final class EventUtil {
             .queryParam("ctz", event.getTimeZone())
             .queryParam("text", event.getDisplayName())
             .queryParam("location", event.getLocation())
-            .queryParam("details", description)
+            .queryParam("details", StringUtils.abbreviate(description, 1024))
             .toUriString();
     }
 
