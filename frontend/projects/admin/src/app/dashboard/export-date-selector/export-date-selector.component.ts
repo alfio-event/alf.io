86508b53c3e5c8d5bfd03114fe875343a9a99e37
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-05-26 09:31:16 +0200

    Add logic for date

--- frontend/projects/admin/src/app/dashboard/export-date-selector/export-date-selector.component.ts
+++ frontend/projects/admin/src/app/dashboard/export-date-selector/export-date-selector.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,10 +9,25 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./export-date-selector.component.scss'],
 })
 export class ExportDateSelectorComponent implements OnInit {
+  public dateForm: FormGroup;
   constructor(
     private readonly modalService: NgbModal,
-    public activeModal: NgbActiveModal
-  ) {}
+    public activeModal: NgbActiveModal,
+    formBuilder: FormBuilder
+  ) {
+    this.dateForm = formBuilder.group({
+      fromDate: [null, Validators.required],
+      toDate: [null, Validators.required],
+    });
+  }
+
+  get fromDate() {
+    return this.dateForm.get('fromDate');
+  }
+
+  get toDate() {
+    return this.dateForm.get('toDate');
+  }
 
   ngOnInit(): void {}
 }
