4322027a37000cc7a74a3430d532c5c51c18d448
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-06-01 13:54:23 +0200

    Added cards

--- frontend/projects/admin/src/app/event/event-dashboard/event-dashboard.component.ts
+++ frontend/projects/admin/src/app/event/event-dashboard/event-dashboard.component.ts
@@ -1,23 +1,26 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {map, Observable, of, switchMap} from 'rxjs';
-import {EventService} from "../../shared/event.service";
-import {Event} from "../../model/event";
-
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, of, switchMap } from 'rxjs';
+import { EventService } from '../../shared/event.service';
+import { Event } from '../../model/event';
+import { BarChart, PieChart } from 'chartist';
 
 @Component({
   selector: 'app-event-dashboard',
   templateUrl: './event-dashboard.component.html',
-  styleUrls: ['./event-dashboard.component.scss']
+  styleUrls: ['./event-dashboard.component.scss'],
 })
 export class EventDashboardComponent {
-
   public eventId$: Observable<string | null> = of();
   public event$: Observable<Event | null> = of();
 
-  constructor(private readonly eventService: EventService, route: ActivatedRoute) {
+  constructor(
+    private readonly eventService: EventService,
+    route: ActivatedRoute
+  ) {
     this.eventId$ = route.paramMap.pipe(map((pm) => pm.get('eventId')));
-    this.event$ = this.eventId$.pipe(switchMap(v => v != null ? eventService.getEvent(v) : of()));
+    this.event$ = this.eventId$.pipe(
+      switchMap((v) => (v != null ? eventService.getEvent(v) : of()))
+    );
   }
-
 }
