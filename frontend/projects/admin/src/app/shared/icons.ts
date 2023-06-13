d3b03a32874ea3919dd15ae4a0d06c0563b4d367
Lorena Mondini <60662357+Lorslux@users.noreply.github.com>
2023-06-13 14:08:44 +0200

    list menu with icons

--- frontend/projects/admin/src/app/shared/icons.ts
+++ frontend/projects/admin/src/app/shared/icons.ts
@@ -2,28 +2,34 @@ import { accessIcon } from '../svg/access';
 import { accountcircleIcon } from '../svg/accountcircle';
 import { addIcon } from '../svg/add';
 import { addCircleNewIcon } from '../svg/add-circle-new';
+import { arrowBackIcon } from '../svg/arrow-back';
 import { arrowdropdownIcon } from '../svg/arrowdropdown';
 import { calendarEventIcon } from '../svg/calendar-event';
 import { categoryIcon } from '../svg/category';
 import { checkIcon } from '../svg/check';
 import { deleteIcon } from '../svg/delete';
+import { descriptionIcon } from '../svg/description';
 import { detailsIcon } from '../svg/details';
 import { downloadIcon } from '../svg/download';
 import { editIcon } from '../svg/edit';
+import { emailSentIcon } from '../svg/email-sent';
 import { eventIcon } from '../svg/event';
 import { groupsIcon } from '../svg/groups';
 import { homeIcon } from '../svg/home';
 import { hourglassTopIcon } from '../svg/hourglass-top';
+import { insightsIcon } from '../svg/insights';
 import { keyIcon } from '../svg/key';
 import { lockOpenIcon } from '../svg/lock-open';
 import { organizationIcon } from '../svg/organization';
 import { paymentsIcon } from '../svg/payments';
+import { percentIcon } from '../svg/percent';
 import { piggyBankIcon } from '../svg/piggy-bank';
 import { resetIcon } from '../svg/reset';
 import { searchIcon } from '../svg/search';
 import { settingsIcon } from '../svg/settings';
 import { subscriptionIcon } from '../svg/subscription';
 import { ticketIcon } from '../svg/ticket';
+import { ticketReservedIcon } from '../svg/ticket-reserved';
 import { visibilityOnIcon } from '../svg/visibility-on';
 import { visibilityoffIcon } from '../svg/visibilityoff';
 
@@ -32,28 +38,34 @@ const ICONS = [
   accountcircleIcon,
   addCircleNewIcon,
   addIcon,
+  arrowBackIcon,
   arrowdropdownIcon,
   calendarEventIcon,
   categoryIcon,
   checkIcon,
   deleteIcon,
+  descriptionIcon,
   detailsIcon,
   downloadIcon,
   editIcon,
+  emailSentIcon,
   eventIcon,
   groupsIcon,
   homeIcon,
   hourglassTopIcon,
+  insightsIcon,
   keyIcon,
   lockOpenIcon,
   organizationIcon,
   paymentsIcon,
+  percentIcon,
   piggyBankIcon,
   resetIcon,
   searchIcon,
   settingsIcon,
   subscriptionIcon,
   ticketIcon,
+  ticketReservedIcon,
   visibilityoffIcon,
   visibilityOnIcon,
 ];
