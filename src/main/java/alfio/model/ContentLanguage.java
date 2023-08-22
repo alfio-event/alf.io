080ed7fea138ae7d1620360ec6592d713ed77a5b
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2023-08-22 08:03:25 +0200

    integrate Czech (CS) + update NL and FR

--- src/main/java/alfio/model/ContentLanguage.java
+++ src/main/java/alfio/model/ContentLanguage.java
@@ -40,8 +40,9 @@ public class ContentLanguage {
     public static final ContentLanguage DANISH = new ContentLanguage(LocaleUtil.forLanguageTag("da"),0b10000000000, LocaleUtil.forLanguageTag("da"));
     public static final ContentLanguage BULGARIAN = new ContentLanguage(LocaleUtil.forLanguageTag("bg"),0b100000000000, LocaleUtil.forLanguageTag("bg"));
     public static final ContentLanguage SWEDISH = new ContentLanguage(LocaleUtil.forLanguageTag("sv"),0b1000000000000, LocaleUtil.forLanguageTag("sv"));
+    public static final ContentLanguage CZECH = new ContentLanguage(LocaleUtil.forLanguageTag("cs"),0b10000000000000, LocaleUtil.forLanguageTag("cs"));
 
-    public static final List<ContentLanguage> ALL_LANGUAGES = List.of(ENGLISH, SPANISH, ITALIAN, GERMAN, DUTCH, FRENCH, ROMANIAN, PORTUGUESE, TURKISH, POLISH, DANISH, BULGARIAN, SWEDISH);
+    public static final List<ContentLanguage> ALL_LANGUAGES = List.of(ENGLISH, SPANISH, ITALIAN, GERMAN, DUTCH, FRENCH, ROMANIAN, PORTUGUESE, TURKISH, POLISH, DANISH, BULGARIAN, SWEDISH, CZECH);
     public static final int ALL_LANGUAGES_IDENTIFIER = ALL_LANGUAGES.stream().mapToInt(ContentLanguage::getValue).reduce(0, (a,b) -> a|b);
 
     public static List<ContentLanguage> findAllFor(int bitMask) {
