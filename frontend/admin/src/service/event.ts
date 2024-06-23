7dc839a8ffb43e14576421465ce4a899d2021ee5
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2024-06-23 16:57:28 +0200

    apply suggested modifications

--- frontend/admin/src/service/event.ts
+++ frontend/admin/src/service/event.ts
@@ -1,8 +1,8 @@
 import {EventWithOrganization} from "../model/event.ts";
+import {fetchJson} from "./helpers.ts";
 
 export class EventService {
     static load(publicIdentifier: string): Promise<EventWithOrganization> {
-        return fetch(`/admin/api/events/${publicIdentifier}`)
-            .then(response => response.json());
+        return fetchJson(`/admin/api/events/${publicIdentifier}`);
     }
 }
