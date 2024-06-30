7dbe4dfdaf3558402b7b475f94a6687bb52998f5
Sylvain Jermini <sylvain.jermini@syjer.com>
2024-06-30 21:54:57 +0200

    i18n admin

---
+++ frontend/admin/src/generated/locales/it.ts
@@ -0,0 +1,23 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      'h8dbae751b47cca9d': html`Powered by <a href="https://alf.io" target="_blank">Alf.io</a> v.${0}`,
+'hf8ae7043408fe50a': html`<a href="https://github.com/alfio-event/alf.io/issues" target="_blank" rel="noopener">report an issue</a> or <a href="https://github.com/alfio-event/alf.io/discussions" target="_blank" rel="nofollow noopener noreferrer">ask for help</a>`,
+'s66f7fb1ee5abad1b': `Tell us about you!`,
+'s453bbc36b1e1022b': `Thank you for using Alf.io!`,
+'scd07ca3518e4f6e4': `We don't track usage in any way, because we respect your privacy`,
+'sefdd01e68dc17845': `...but we're very happy that you've decided to use Alf.io and we'd love to hear your story!`,
+'sf4b7b3fc180a2259': `Contribute on OpenCollective`,
+'s12d6dde9b30c3093': `Dismiss`,
+'s902283dfce75a674': `Your account has been deleted`,
+'saf7a91f4fd57c858': `We're very sorry to see you go!`,
+    };
+  
