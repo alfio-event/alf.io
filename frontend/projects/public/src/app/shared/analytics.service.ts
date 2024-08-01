668570005646115c28b791c55fcde585d9ff5c2f
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2024-08-01 09:15:26 +0200

    add support for GA4

--- frontend/projects/public/src/app/shared/analytics.service.ts
+++ frontend/projects/public/src/app/shared/analytics.service.ts
@@ -1,55 +1,77 @@
-import {Injectable} from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {AnalyticsConfiguration} from '../model/analytics-configuration';
 import {Observable} from 'rxjs';
 
+const initAttribute = 'data-alfio-init-complete';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AnalyticsService {
 
-  private gaScript: Observable<[Function, AnalyticsConfiguration, string]> = null;
+    private gaScript: Observable<[Function, AnalyticsConfiguration, string]> = null;
 
-  constructor() {
-  }
-
-  pageView(conf: AnalyticsConfiguration): void {
-    const locationPathName = location.pathname;
-    if (conf.googleAnalyticsKey) {
-      this.handleGoogleAnalytics(conf, locationPathName);
+    constructor() {
     }
-  }
 
-
-  private handleGoogleAnalytics(conf: AnalyticsConfiguration, locationPathName: string) {
-    if (this.gaScript === null) {
-      this.gaScript = new Observable<[Function, AnalyticsConfiguration, string]>(subscribe => {
-        if (!document.getElementById('GA_SCRIPT')) { // <- script is not created
-          const scriptElem = document.createElement('script');
-          scriptElem.id = 'GA_SCRIPT';
-          scriptElem.addEventListener('load', () => {
-            subscribe.next([window['ga'], conf, locationPathName]);
-          });
-          scriptElem.src = 'https://ssl.google-analytics.com/analytics.js';
-          scriptElem.async = true;
-          scriptElem.defer = true;
-          document.body.appendChild(scriptElem);
-        } else if (!window['ga']) { // <- script has been created, but not loaded
-          document.getElementById('GA_SCRIPT').addEventListener('load', () => {
-            subscribe.next([window['ga'], conf, locationPathName]);
-          });
-        } else { // <- script has been loaded
-          subscribe.next([window['ga'], conf, locationPathName]);
+    pageView(conf: AnalyticsConfiguration): void {
+        const locationPathName = location.pathname;
+        if (conf.googleAnalyticsKey) {
+            this.handleGoogleAnalytics(conf, locationPathName);
         }
-      });
     }
 
-    this.gaScript.subscribe(([ga, configuration, pathname]) => {
-      if (configuration.googleAnalyticsScrambledInfo) {
-        ga('create', configuration.googleAnalyticsKey, {'anonymizeIp': true, 'storage': 'none', 'clientId': configuration.clientId});
-      } else {
-        ga('create', configuration.googleAnalyticsKey);
-      }
-      ga('send', 'pageview', pathname);
-    });
-  }
+
+    private handleGoogleAnalytics(conf: AnalyticsConfiguration, locationPathName: string) {
+        if (this.gaScript === null) {
+            this.gaScript = new Observable<[Function, AnalyticsConfiguration, string]>(subscribe => {
+                const script = document.getElementById('GA_SCRIPT');
+                if (script == null) { // <- script is not created
+                    const scriptElem = document.createElement('script');
+                    scriptElem.id = 'GA_SCRIPT';
+                    scriptElem.addEventListener('load', () => {
+                        subscribe.next([this.initGtag(conf, scriptElem), conf, locationPathName]);
+                    });
+                    scriptElem.src = `https://www.googletagmanager.com/gtag/js?id=${conf.googleAnalyticsKey}`;
+                    scriptElem.async = true;
+                    document.head.appendChild(scriptElem);
+                } else if (script.getAttribute(initAttribute) == null) { // <- script has been created, but not loaded
+                    subscribe.next([this.initGtag(conf, script), conf, locationPathName]);
+                } else { // <- script has been loaded
+                    subscribe.next([window['gtag'], conf, locationPathName]);
+                }
+            });
+        }
+
+        this.gaScript.subscribe(([gtag, config, pathname]) => {
+            if (isDevMode()) {
+                console.log('calling gtag for url', pathname);
+            }
+            gtag('config', config.googleAnalyticsKey, { 'page_path': pathname });
+        });
+    }
+
+    private initGtag(configuration: AnalyticsConfiguration, script: HTMLElement): any {
+        if (isDevMode()) {
+            console.log('init gtag', configuration.googleAnalyticsKey);
+        }
+        window['dataLayer'] = window['dataLayer'] || [];
+        window['gtag'] = window['gtag'] || function () {
+            window['dataLayer'].push(arguments as any);
+        };
+        const gtag = window['gtag'];
+        if (configuration.googleAnalyticsScrambledInfo) {
+            // deny everything
+            gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied'
+            });
+        }
+        gtag('js', new Date());
+        gtag('config', configuration.googleAnalyticsKey);
+        script.setAttribute(initAttribute, 'true');
+        return gtag;
+    }
 }
