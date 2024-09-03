b64c07d290e521c25f3b6f211f518961b811fc31
Sylvain Jermini <sylvain.jermini@syjer.com>
2024-09-03 19:59:00 +0200

    cleanup import

--- src/main/java/alfio/manager/TicketCategoryAvailabilityManager.java
+++ src/main/java/alfio/manager/TicketCategoryAvailabilityManager.java
@@ -37,8 +37,6 @@ import alfio.repository.TicketCategoryRepository;
 import alfio.util.ClockProvider;
 import alfio.util.EventUtil;
 import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
