91eb2ccabf45c3b535a4fb44fea5e1518bd2a73e
Dimi <dimitrisli@gmail.com>
2024-11-23 15:40:25 +0100

    Supporting External Reference in List of Attendees Download
    Closes #1422

--- src/test/java/alfio/controller/api/admin/EventApiControllerIntegrationTest.java
+++ src/test/java/alfio/controller/api/admin/EventApiControllerIntegrationTest.java
@@ -20,19 +20,24 @@ import alfio.TestConfiguration;
 import alfio.config.DataSourceConfiguration;
 import alfio.config.Initializer;
 import alfio.controller.api.ControllerConfiguration;
+import alfio.manager.AdminReservationRequestManager;
 import alfio.manager.EventManager;
 import alfio.manager.user.UserManager;
 import alfio.model.Event;
 import alfio.model.TicketCategory;
 import alfio.model.metadata.AlfioMetadata;
+import alfio.model.modification.AdminReservationModification;
 import alfio.model.modification.DateTimeModification;
 import alfio.model.modification.TicketCategoryModification;
 import alfio.repository.EventDeleterRepository;
 import alfio.repository.EventRepository;
+import alfio.repository.TicketCategoryRepository;
+import alfio.repository.TicketRepository;
 import alfio.repository.system.ConfigurationRepository;
 import alfio.repository.user.OrganizationRepository;
 import alfio.test.util.AlfioIntegrationTest;
 import alfio.test.util.IntegrationTestUtil;
+import alfio.util.ClockProvider;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -46,10 +51,14 @@ import org.springframework.test.context.ContextConfiguration;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static alfio.test.util.IntegrationTestUtil.*;
 import static alfio.test.util.TestUtil.clockProvider;
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -73,6 +82,14 @@ class EventApiControllerIntegrationTest {
     private EventDeleterRepository eventDeleterRepository;
     @Autowired
     private EventApiController eventApiController;
+    @Autowired
+    private AttendeeBulkImportApiController attendeeBulkImportApiController;
+    @Autowired
+    private AdminReservationRequestManager adminReservationRequestManager;
+    @Autowired
+    private TicketCategoryRepository ticketCategoryRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
 
     private Event event;
 
@@ -119,6 +136,40 @@ class EventApiControllerIntegrationTest {
         assertEquals(event.getShortName(), events.get(0).getKey());
     }
 
+    @Test
+    void testListAttendeesWithFieldsUploadAndSameFieldsAvailableOnDownload() {
+        // GIVEN - creation of event and registration of attendees
+        var eventAndUser = createEvent(Event.EventFormat.HYBRID);
+        event = eventAndUser.getKey();
+        var principal = Mockito.mock(Authentication.class);
+        when(principal.getName()).thenReturn(owner(eventAndUser.getValue()));
+        var modification = getTestAdminReservationModification();
+        var result = this.attendeeBulkImportApiController.createReservations(eventAndUser.getKey().getShortName(), modification, false, principal);
+        // GIVEN - invocation of async processing job
+        var requestStatus = this.attendeeBulkImportApiController.getRequestsStatus(eventAndUser.getKey().getShortName(), result.getData(), principal);
+        assertEquals(1, requestStatus.getData().getCountPending());
+        // WHEN - processing of pending reservations completes
+        this.adminReservationRequestManager.processPendingReservations();
+        // THEN
+        var tickets = this.ticketRepository.findAllConfirmedForCSV(event.getId());
+        assertEquals(1, tickets.size());
+        var foundTicket = tickets.get(0).getTicket();
+        assertEquals("123", foundTicket.getExtReference());
+        assertEquals("Attendee 0", foundTicket.getFirstName());
+        assertEquals("Test0", foundTicket.getLastName());
+        assertEquals("attendee0@test.ch", foundTicket.getEmail());
+        assertEquals("en", foundTicket.getUserLanguage());
+    }
+
+    private AdminReservationModification getTestAdminReservationModification() {
+        DateTimeModification expiration = DateTimeModification.fromZonedDateTime(ZonedDateTime.now(ClockProvider.clock()).plusDays(1));
+        AdminReservationModification.CustomerData customerData = new AdminReservationModification.CustomerData("Integration", "Test", "integration-test@test.ch", "Billing Address", "reference", "en", "1234", "CH", null);
+        var ticketCategoryList = this.ticketCategoryRepository.findAllTicketCategories(event.getId());
+        AdminReservationModification.Category category = new AdminReservationModification.Category(ticketCategoryList.get(0).getId(), "name", new BigDecimal("100.00"), null);
+        List<AdminReservationModification.TicketsInfo> ticketsInfoList = Collections.singletonList(new AdminReservationModification.TicketsInfo(category, generateAttendees(1), true, false));
+        return new AdminReservationModification(expiration, customerData, ticketsInfoList, "en", false, false, null, null, null, null);
+    }
+
     private Pair<Event,String> createEvent(Event.EventFormat format) {
         IntegrationTestUtil.ensureMinimalConfiguration(configurationRepository);
         List<TicketCategoryModification> categories = List.of(
@@ -129,6 +180,12 @@ class EventApiControllerIntegrationTest {
         );
         return initEvent(categories, organizationRepository, userManager, eventManager, eventRepository, List.of(), format);
 
+    }
+
+    private List<AdminReservationModification.Attendee> generateAttendees(int count) {
+        return IntStream.range(0, count)
+            .mapToObj(i -> new AdminReservationModification.Attendee(null, "Attendee "+i, "Test" + i, "attendee"+i+"@test.ch", "en",false, "123", null, Collections.emptyMap(), null))
+            .collect(toList());
     }
 
     @AfterEach
