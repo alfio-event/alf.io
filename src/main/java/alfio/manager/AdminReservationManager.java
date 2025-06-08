9ff43704184f3b7c23caeb028b9d26c99cf852eb
Shane Brown <contact@shanebrown.ca>
2025-06-08 13:58:11 -0400

    Enable credit/confirm/refund options in admin for custom payments
    When viewing a reservation in the admin panel which is a bank transfer,
    there are extra options to allow an admin to confirm payments, credit a
    reservation, or refund a portion of the reservation payment. These
    options were not previously available for custom offline payments. This
    commit re-enables that functionality.

--- src/main/java/alfio/manager/AdminReservationManager.java
+++ src/main/java/alfio/manager/AdminReservationManager.java
@@ -947,7 +947,11 @@ public class AdminReservationManager {
     public void creditReservation(PurchaseContextType purchaseContextType, String publicIdentifier, String reservationId, boolean refund, boolean notify, String username) {
         loadReservation(purchaseContextType, publicIdentifier, reservationId, username)
             .ifSuccess(res -> {
-                if (res.getLeft().getStatus() == TicketReservationStatus.OFFLINE_PAYMENT) {
+                var OFFLINE_PAYMENT_ENUM_VALUES = EnumSet.of(
+                    TicketReservationStatus.OFFLINE_PAYMENT,
+                    TicketReservationStatus.CUSTOM_OFFLINE_PAYMENT
+                );
+                if (OFFLINE_PAYMENT_ENUM_VALUES.contains(res.getLeft().getStatus())) {
                     ticketReservationManager.deleteOfflinePayment(res.getRight().event().orElseThrow(), reservationId, false, true, notify, username);
                 } else {
                     removeReservation(res, refund, notify, username, false, true);
